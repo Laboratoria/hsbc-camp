f20b7ba7ec65916c8d7f2a784341aadc1f03580f
Sebastian Silva <sebastian@fuentelibre.org>
2025-02-13 12:17:52 -0500

    Add material

---
+++ tests/Prueba.java
@@ -0,0 +1,24 @@
+// package tests;
+
+class Prueba {
+    
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
+        if (year % 100 == 0) {
+            return false;
+        }
+        if (year % 4 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(2000)); // true
+        System.out.println(isLeapYear(1900)); // false
+        System.out.println(isLeapYear(2004)); // true
+        System.out.println(isLeapYear(2005)); // false
+    }
+}
